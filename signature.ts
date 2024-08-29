8615af8aac6f47622ff4f264fbf26946182adbf2
Jan Miksovsky <jan@miksovsky.com>
2024-08-29 15:00:44 -0700

    Initial work

---
+++ signature.ts
@@ -0,0 +1,7 @@
+/**
+ * Destructively modify the `project` object according to the domain-specific
+ * language in the object's `options` property .
+ */
+export default async function tool(project: any): Promise<void> {
+  /* Stuff happens here… */
+}
